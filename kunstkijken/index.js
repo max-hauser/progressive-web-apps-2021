829d718f18e450a1039c32818612972c8b84ebb0
Max Hauser <hauser.m97@gmail.com>
2021-03-11 22:52:23 +0100

    fixed issues and it kinda works now (sort of)

--- kunstkijken/index.js
+++ kunstkijken/index.js
@@ -1,61 +1,43 @@
 // Require third-party modules
 const express = require('express');
-const request = require('request');
-const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 // Config object
 const config = { port: 3000 }
-
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 app.use(express.static('public'));
 
-// use bodyParser
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
+const endpoint = 'https://www.rijksmuseum.nl/api/nl/collection/';
+const key = 'SM7rr6VN';
+
+// Actually set up the server
+app.listen(config.port, function() {
+	console.log(`Application started on port: ${config.port}`);
+});
 
 // Create a route for our overview page
 app.get('/', function(req, res) {
-	const endpoint = 'https://www.rijksmuseum.nl/api/nl/collection/';
-	const key = 'SM7rr6VN';
 	const url = `${endpoint}?key=${key}`;	
-	request(`${url}`, {json: true}, function (err, requestRes, body){
-		if (err) {
-			res.send(err);
-		} else {
-			res.render('posts', {
-				title: 'Posts', 
-				postData: body.artObjects
-			});
-		}
+	fetch(url).then(res => res.json()).then(data => {
+		res.render('posts', {
+			title: 'Posts', // We use this for the page title, see views/partials/head.ejs
+			postData: data
+		});	
 	});
 });
 
 // Create a route for our detail page
 app.get('/post/:id', function(req, res) {
-
-	const endpoint = 'https://www.rijksmuseum.nl/api/nl/collection';
-	const key = 'SM7rr6VN';	
 	const url = `${endpoint}/${req.params.id}?key=${key}`;
-	
-	console.log(url)
-
-	request(url, {json: true}, function (err, requestRes, body){
-		if (err) {
-			// We got an error
-			res.send(err);
-		} else {
-			console.log(body);
-			//Render the page using the 'post' view and our body data
-			res.render('post', {
-				title: `Post ${req.params.id}`, 
-				postData: body.artObject
-			});
-		}
-	});
+	fetch(url).then(res => res.json()).then(data => {
+		res.render('post', {
+			title: 'Post ${req.params.id}',
+			postData: data.artObject
+		});	
+	});	
 });
 
 // Make sure to catch /post to /posts
@@ -64,7 +46,3 @@ app.get('/post', function(req, res) {
 	res.redirect('/posts');
 });
 
-// Actually set up the server
-app.listen(config.port, function() {
-	console.log(`Application started on port: ${config.port}`);
-});
